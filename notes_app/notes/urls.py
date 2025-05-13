0bcb59128aac2c51172c6adc9fa14261ca001a52
Kostiantyn Demarov <93338028+WonderliP@users.noreply.github.com>
2025-05-13 19:37:17 +0300

    Add files via upload

--- notes_app/notes/urls.py
+++ notes_app/notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import greeting
+from .views import greeting, create_notes
 
 urlpatterns = [
-    path("greet/", greeting)
+    path("greet/", greeting),
+    path("notes/", create_notes)
 ]
--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -5,3 +5,26 @@ from django.http import HttpResponse
 # Create your views here.
 def greeting(request):
     return HttpResponse("Hello from Notes app")
+
+
+def create_notes(request):
+    notes = [
+        {
+            "title": "First Note",
+            "content": "This is a content of first note."
+        }, {
+            "title": "Second Note",
+            "content": "This is a content of second note with some important information."
+        }, {
+            "title": "Reminder",
+            "content": "To buy bread, milk and coffee."
+        }, {
+            "title": "Idea of the project",
+            "content": "To create a task management app."
+        }, {
+            "title": "Quote of the day",
+            "content": '"Everything that does not kill us makes us stronger." - Nietzsche.'
+        },
+    ]
+
+    return render(request, "index.html", {"notes": notes})
