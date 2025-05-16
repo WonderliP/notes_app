f747a5dfc1bd234bcdc089d354fb34c46a6235bb
Kostiantyn Demarov <93338028+WonderliP@users.noreply.github.com>
2025-05-16 21:29:39 +0300

    Add files via upload

--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from notes.models import Note
+
 
 # Create your views here.
 def greeting(request):
@@ -8,23 +10,5 @@ def greeting(request):
 
 
 def create_notes(request):
-    notes = [
-        {
-            "title": "First Note",
-            "content": "This is a content of first note."
-        }, {
-            "title": "Second Note",
-            "content": "This is a content of second note with some important information."
-        }, {
-            "title": "Reminder",
-            "content": "To buy bread, milk and coffee."
-        }, {
-            "title": "Idea of the project",
-            "content": "To create a task management app."
-        }, {
-            "title": "Quote of the day",
-            "content": '"Everything that does not kill us makes us stronger." - Nietzsche.'
-        },
-    ]
-
+    notes = Note.objects.all()
     return render(request, "index.html", {"notes": notes})
