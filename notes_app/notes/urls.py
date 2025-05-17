50a88e0bc1530acf131e2b73436cb8573bb571df
Kostiantyn Demarov <93338028+WonderliP@users.noreply.github.com>
2025-05-17 13:57:12 +0300

    Add files via upload

--- notes_app/notes/urls.py
+++ notes_app/notes/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import greeting, create_notes
+from .views import greeting, create_notes, NoteCreateView, NoteDetailView, NoteUpdateView, NoteDeleteView, NoteListView
 
 urlpatterns = [
     path("greet/", greeting),
-    path("notes/", create_notes)
+    path("notes/", create_notes),
+
+    path("", NoteListView.as_view(), name="note_list"),
+    path("create/", NoteCreateView.as_view(), name="note_create"),
+    path("<int:pk>/", NoteDetailView.as_view(), name="note_detail"),
+    path("<int:pk>/edit/", NoteUpdateView.as_view(), name="note_update"),
+    path("<int:pk>/delete/", NoteDeleteView.as_view(), name="note_delete"),
 ]
--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
+from notes.forms import NoteForm
 from notes.models import Note
 
 
@@ -12,3 +15,37 @@ def greeting(request):
 def create_notes(request):
     notes = Note.objects.all()
     return render(request, "index.html", {"notes": notes})
+
+
+class NoteCreateView(CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "note_form.html"
+    success_url = reverse_lazy("note_list")
+
+
+class NoteDetailView(DetailView):
+    model = Note
+    template_name = "note_detail.html"
+
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "note_form.html"
+    success_url = reverse_lazy("note_list")
+
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    template_name = "note_confirm_delete.html"
+    success_url = reverse_lazy("note_list")
+
+
+class NoteListView(ListView):
+    model = Note
+    template_name = "note_list.html"
+    context_object_name = "notes"
+
+    def get_queryset(self):
+        return super().get_queryset()
